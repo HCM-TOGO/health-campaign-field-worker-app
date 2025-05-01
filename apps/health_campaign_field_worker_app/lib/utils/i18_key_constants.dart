86fa17ed696b93012f4aa5c8623cc7ffd1871298
psbskb <psb.skb22@gmail.com>
2025-05-02 02:54:32 +0530

    householdId const added

--- apps/health_campaign_field_worker_app/lib/utils/i18_key_constants.dart
+++ apps/health_campaign_field_worker_app/lib/utils/i18_key_constants.dart
@@ -1145,10 +1145,11 @@ class IndividualDetails {
 class BeneficiaryDetails {
   const BeneficiaryDetails();
   String get insufficientStockHeading => 'INSUFFICIENT_STOCK_HEADING';
-String get insufficientAZTStockMessage => 'INSUFFICIENT_AZT_STOCK_MESSAGE';
-  String get insufficientAZTStockMessageDelivery =>   
+  String get insufficientAZTStockMessage => 'INSUFFICIENT_AZT_STOCK_MESSAGE';
+  String get insufficientAZTStockMessageDelivery =>
       'INSUFFICIENT_AZT_STOCK_MESSAGE_DELIVERY';
 
+  String get householdId => 'HOUSEHOLD_ID';
   String get beneficiaryId => 'BENEFICIARY_ID';
   String get backToHouseholdDetails => 'BACK_TO_HOUSEHOLD_DETAILS';
   String get beneficiaryDoseUnit => 'BENEFICIARY_DETAILS_DOSE_UNIT';
