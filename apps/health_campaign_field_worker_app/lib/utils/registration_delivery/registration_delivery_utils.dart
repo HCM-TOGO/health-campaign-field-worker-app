1dd85b38ddf8c6dfc0e72b987d4d29c9b7641747
aviral-beehyv <aviral.gupta@beehyv.com>
2025-05-10 12:10:39 +0530

    added material note number generator

--- apps/health_campaign_field_worker_app/lib/utils/registration_delivery/registration_delivery_utils.dart
+++ apps/health_campaign_field_worker_app/lib/utils/registration_delivery/registration_delivery_utils.dart
@@ -50,4 +50,46 @@ class UniqueIdGeneration {
         ? {formattedUniqueId, combinedId}
         : {formattedUniqueId};
   }
+
+  Future<Set<String>> generateUniqueMaterialNoteNumber({
+    required String loggedInUserId,
+    required bool returnCombinedIds,
+  }) async {
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+
+    String androidId = androidInfo.serialNumber == 'unknown'
+        ? androidInfo.id.replaceAll('.', '')
+        : androidInfo.serialNumber;
+
+    int timestamp = DateTime.now().millisecondsSinceEpoch;
+
+    String combinedId = '$loggedInUserId$androidId$timestamp';
+
+    // Generate SHA-256 hash
+    List<int> bytes = utf8.encode(combinedId);
+    Digest sha256Hash = sha256.convert(bytes);
+
+    // Convert the hash to a 12-character string and make it uppercase
+    String hashString = sha256Hash.toString();
+    String uniqueId = hashString.substring(0, 12).toUpperCase();
+
+    // Add a hyphen every 4 characters
+    String formattedUniqueId = uniqueId.replaceAllMapped(
+      RegExp(r'.{1,4}'),
+      (match) => '${match.group(0)}-',
+    );
+
+    // Remove the last hyphen
+    formattedUniqueId =
+        formattedUniqueId.substring(0, formattedUniqueId.length - 1);
+
+    if (kDebugMode) {
+      print('uniqueId : $formattedUniqueId');
+    }
+
+    return returnCombinedIds
+        ? {formattedUniqueId, combinedId}
+        : {formattedUniqueId};
+  }
 }
