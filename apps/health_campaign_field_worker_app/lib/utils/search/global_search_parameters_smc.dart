6851e1ede9de724ccff65c61fec76fa60130a404
amitverma-egov <amit.verma@egovernments.org>
2025-05-13 22:20:28 +0530

    add beneficiary id search and removed showHelp from some pages

---
+++ apps/health_campaign_field_worker_app/lib/utils/search/global_search_parameters_smc.dart
@@ -0,0 +1,26 @@
+class GlobalSearchParametersSMC {
+  final bool isProximityEnabled;
+  final double? latitude;
+  final String? projectId;
+  final double? longitude;
+  final double? maxRadius;
+  final String? nameSearch;
+  final String? beneficiaryId;
+  final int? offset;
+  final int? limit;
+  final List<String>? filter;
+  final int? totalCount;
+
+  GlobalSearchParametersSMC(
+      {required this.isProximityEnabled,
+      required this.latitude,
+      required this.longitude,
+      required this.maxRadius,
+      required this.nameSearch,
+      required this.beneficiaryId,
+      required this.offset,
+      required this.limit,
+      required this.filter,
+      this.totalCount,
+      this.projectId});
+}
