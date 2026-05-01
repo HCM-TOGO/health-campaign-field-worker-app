b20c3a4497014b263076d54f015889c22e5acaa9
nikhilesh <nikhilesh@beehyv.com>
2026-05-01 17:28:11 +0530

    disabled downsync

--- apps/health_campaign_field_worker_app/lib/utils/constants.dart
+++ apps/health_campaign_field_worker_app/lib/utils/constants.dart
@@ -139,7 +139,7 @@ class Constants {
   static const String pendingEligible = 'pendingEligible';
 
   // todo enable before cycle2
-  static const bool isDownSyncEnabled = true;
+  static const bool isDownSyncEnabled = false;
   static const String dashboardAnalyticsPath =
       '/dashboard-analytics/dashboard/getChartV2';
 
