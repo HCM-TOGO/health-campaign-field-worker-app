b014320b1461d4987c4f9954cd194405ebd22102
nikhilesh <nikhilesh@beehyv.com>
2026-05-01 17:25:50 +0530

    update delivery team name for stocks search

--- apps/health_campaign_field_worker_app/lib/pages/inventory/custom_stock_details.dart
+++ apps/health_campaign_field_worker_app/lib/pages/inventory/custom_stock_details.dart
@@ -930,7 +930,7 @@ class CustomStockDetailsPageState
                                                       null &&
                                                   element.receiverId ==
                                                       (deliveryTeamSelected
-                                                          ? 'FAC_${selectedFacilityId}'
+                                                          ? deliveryTeamName
                                                           : selectedFacilityId) &&
                                                   element.auditDetails
                                                           ?.createdBy ==
@@ -968,9 +968,10 @@ class CustomStockDetailsPageState
                                                       //           .pipeSeparator)
                                                       //       .last
                                                       // ],
-                                                      senderId: deliveryTeamSelected
-                                                          ? 'FAC_${selectedFacilityId}'
-                                                          : selectedFacilityId,
+                                                      senderId:
+                                                          deliveryTeamSelected
+                                                              ? deliveryTeamName
+                                                              : selectedFacilityId,
                                                       transactionType: [
                                                         TransactionType.received
                                                             .toValue()
