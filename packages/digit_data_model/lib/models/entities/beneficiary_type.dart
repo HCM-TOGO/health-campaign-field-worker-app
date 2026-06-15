15e74fd5ee03b95bfe0e47ece4e7c6b00ef9694c
Harsh Khatri <harsh.khatri@beehyv.com>
2026-06-15 20:29:11 +0530

    Digit data model addition for the new beneficiary type of 0-2MONTH

---
+++ packages/digit_data_model/lib/models/entities/beneficiary_type.dart
@@ -0,0 +1,39 @@
+// Generated using mason. Do not modify by hand
+import 'package:dart_mappable/dart_mappable.dart';
+
+part 'beneficiary_type.mapper.dart';
+
+@MappableEnum(caseStyle: CaseStyle.upperCase)
+enum BeneficiaryType {
+  @MappableValue("INDIVIDUAL")
+  individual,
+  @MappableValue("HOUSEHOLD")
+  household,
+  @MappableValue("PRODUCT")
+  product,
+  @MappableValue("3-11MONTH")
+  individual1,
+  @MappableValue("12-59MONTH")
+  individual2,
+  @MappableValue("1-11MONTH")
+  individual3,
+  @MappableValue("6-11MONTH")
+  individual4,
+  @MappableValue("0-2MONTH")
+  individual5,
+  @MappableValue("SPAQ1")
+  spaq1,
+  @MappableValue("SPAQ2")
+  spaq2,
+  @MappableValue("SPAQ-1")
+  spaq3,
+  @MappableValue("SPAQ-2")
+  spaq4,
+  @MappableValue("AZM")
+  azm,
+  @MappableValue("VAS-RED")
+  vasRed,
+  @MappableValue("VAS-BLUE")
+  vasBlue,
+  ;
+}
